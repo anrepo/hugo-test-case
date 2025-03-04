1b7098f15a24813f9fe2d7dc34d9866e9120bf73
Apol <andrey.polenok@gmail.com>
2025-03-04 16:18:44 +0300

    add apo; theme

---
+++ themes/apol/static/js/modules/menu-activity.js
@@ -0,0 +1,28 @@
+let navMenu = (function () {
+  const urlPath = location.pathname;
+
+  function init() {
+    const links = document.querySelectorAll(".nav-menu-link");
+
+    console.log(urlPath);
+
+    for (let item of links) {
+      item.addEventListener("click", () => {
+        clear(links);
+        item.classList.add("nav-menu__active");
+      });
+    }
+  }
+
+  function clear(links) {
+    for (let item of links) {
+      item.classList.remove("nav-menu__active");
+    }
+  }
+
+  return {
+    init: init,
+  };
+})();
+
+export { navMenu };
